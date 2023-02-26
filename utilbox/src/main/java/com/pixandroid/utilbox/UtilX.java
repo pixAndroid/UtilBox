4919a6b014ae0f2f311368869b2d60f103616033
Subhojit <subhojitdp@gmail.com>
2023-02-26 18:06:13 +0530

    Initial commit

---
+++ utilbox/src/main/java/com/pixandroid/utilbox/UtilX.java
@@ -0,0 +1,13 @@
+package com.pixandroid.utilbox;
+
+
+/********************************************
+ *     Created by DailyCoding on 26-Feb-23.  *
+ ********************************************/
+
+public class UtilX {
+
+    public static void setData() {
+
+    }
+}
