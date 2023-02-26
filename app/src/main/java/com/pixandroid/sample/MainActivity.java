db41b4f156082be64ef1ac55549a62029c8aaedd
Subhojit <subhojitdp@gmail.com>
2023-02-26 23:52:00 +0530

    update

--- app/src/main/java/com/pixandroid/sample/MainActivity.java
+++ app/src/main/java/com/pixandroid/sample/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.pixandroid.utilbox.UtilX;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        UtilX.setData();
 
 
 
